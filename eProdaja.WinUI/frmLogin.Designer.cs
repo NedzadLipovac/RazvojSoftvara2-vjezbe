f541c0cd8507793d7c19af9fda5121e93dc83e20
Nedžad Lipovac <nedzad.lipovac@edu.fit.ba>
2019-12-26 13:20:31 +0100

    error with auth, commit after reset

---
+++ eProdaja.WinUI/frmLogin.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace eProdaja.WinUI
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKorisnickoIme = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLozinka = new System.Windows.Forms.TextBox();
+            this.btnPrijava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtKorisnickoIme
+            // 
+            this.txtKorisnickoIme.Location = new System.Drawing.Point(41, 57);
+            this.txtKorisnickoIme.Name = "txtKorisnickoIme";
+            this.txtKorisnickoIme.Size = new System.Drawing.Size(360, 22);
+            this.txtKorisnickoIme.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "KorisnickoIme:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lozinka:";
+            // 
+            // txtLozinka
+            // 
+            this.txtLozinka.Location = new System.Drawing.Point(41, 123);
+            this.txtLozinka.Name = "txtLozinka";
+            this.txtLozinka.PasswordChar = '*';
+            this.txtLozinka.Size = new System.Drawing.Size(360, 22);
+            this.txtLozinka.TabIndex = 2;
+            this.txtLozinka.TextChanged += new System.EventHandler(this.txtLozinka_TextChanged);
+            // 
+            // btnPrijava
+            // 
+            this.btnPrijava.Location = new System.Drawing.Point(304, 175);
+            this.btnPrijava.Name = "btnPrijava";
+            this.btnPrijava.Size = new System.Drawing.Size(97, 36);
+            this.btnPrijava.TabIndex = 4;
+            this.btnPrijava.Text = "LOGIN";
+            this.btnPrijava.UseVisualStyleBackColor = true;
+            this.btnPrijava.Click += new System.EventHandler(this.btnPrijava_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 234);
+            this.Controls.Add(this.btnPrijava);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLozinka);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKorisnickoIme);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKorisnickoIme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLozinka;
+        private System.Windows.Forms.Button btnPrijava;
+    }
+}
---
+++ eProdaja.WinUI/frmLogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eProdaja.WinUI
+{
+    public partial class frmLogin : Form
+    {
+        ApiService service = new ApiService("Korisnici");
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtLozinka_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btnPrijava_Click(object sender, EventArgs e)
+        {
+            ApiService.username = txtKorisnickoIme.Text;
+            ApiService.password = txtLozinka.Text;
+            try
+            {
+
+               await service.Get<dynamic>(null);
+                frmIndex frm = new frmIndex();
+                frm.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Authentifikacija", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
